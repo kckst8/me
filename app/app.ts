05d036a4414f67a309e4ea99caaf94677bfb4c2e
kckst8 <kckst8@gmail.com>
2016-05-29 16:18:52 -0400

    added dependencies

---
+++ app/app.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.html',
+    styleUrls: ['app/app.css']
+})
+ /**
+ * App class
+ */
+export class App {
+    constructor() {
+        
+    }
+}
