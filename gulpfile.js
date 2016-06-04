e8789a27dc70811d30e40c10c5fca1c5d7c7d301
kckst8 <kckst8@gmail.com>
2016-06-03 20:10:19 -0400

    gulp tasks added, moved dependencies to libs temporarily

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,65 @@
-var gulp        = require('gulp');
-var deploy      = require('gulp-gh-pages');
+const gulp      = require('gulp');
+const deploy    = require('gulp-gh-pages');
+const del       = require('del');
+const tsc       = require('gulp-typescript');
+const tscConfig = require('./tsconfig.json')
+/**
+ * Empty dist dir
+ */
+gulp.task('clean', function () {
+    return del('dist/**/*');
+});
 
 /**
- * Push build to kckst8.github.io master branch
+ * compile and deploy to dist
+ */
+gulp.task('compile', ['clean'], function () {
+  return gulp
+    .src('app/**/*.ts')
+    .pipe(tsc(tscConfig.compilerOptions))
+    .pipe(gulp.dest('dist/app'));
+});
+
+/**
+ * copy dependencies to dist/lib
+ */
+gulp.task('copy:libs',['clean'], function() {
+   return gulp.src([
+        'node_modules/angular2/bundles/angular2-polyfills.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/rxjs/bundles/Rx.js',
+        'node_modules/angular2/bundles/angular2.dev.js',
+        'node_modules/angular2/bundles/router.dev.js'
+       ])
+       .pipe(gulp.dest('dist/lib')) 
+});
+
+/**
+ * copy other static files (html, css, etc.)
+ * TODO move these to seperate tasks
+ */
+gulp.task('copy:content',['clean'], function() {
+    return gulp.src([
+        'app/**/*', 
+        'index.html', 
+        'styles.css', 
+        '!app/**/*.ts'
+        ], 
+        { base : './' })
+        .pipe(gulp.dest('dist'))
+});
+
+/**
+ * full build 
+ */
+gulp.task('build', [
+    'compile', 
+    'copy:libs', 
+    'copy:content'
+    ]);
+    
+/**
+ * Push dist build to kckst8.github.io master branch
  */
 var options = { 
     remoteUrl: "https://github.com/kckst8/kckst8.github.io.git",
